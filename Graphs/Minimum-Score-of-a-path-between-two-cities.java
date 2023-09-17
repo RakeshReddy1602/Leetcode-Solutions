757bbdd539a5f508d83076e95b7d2d81f6f0880a
RakeshReddy1602 <rakeshb1602@gmail.com>
2023-09-18 00:30:24 +0530

    BFS Solution with distance array

---
+++ Graphs/Minimum-Score-of-a-path-between-two-cities.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int minScore(int n, int[][] roads) {
+        List<List<Pair>> adj = new ArrayList<>();
+        Queue<Pair> queue = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+        int minCost = Integer.MAX_VALUE;
+        for(int i =0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(inti =0;i<roads.length;i++){
+            int node1 = roads[i][0];
+            int node2 = roads[i][1];
+            int cost = roads[i][2];
+            adj.get(node1).add(new Pair(node2,cost));
+            adj.get(node2).add(new Pair(node1,cost));
+        }
+        queue.add(new Pair(1,Integer.MAX_VALUE));
+        vis[0] = true;
+        while(!queue.isEmpty()){
+            int node = queue.peek().node;
+            minCost = Math.min(minCost,queue.poll().cost);
+            for(Pair p :adj.get(node)){
+                if(!vis[p.node]){
+                    vis[p.node] = true;
+                    queue.add(new Pair(p.node,p.cost));
+
+                }
+            }
+        }
+        return minCost;
+    }
+}
+
+class Pair{
+    int node,cost;
+    Pair(int node,int cost){
+        this.node = node;
+        this.cost = cost;
+    }
+}
