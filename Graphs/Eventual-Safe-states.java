2c43e552a472ec779af6ac69122881082ff8158d
RakeshReddy1602 <rakeshb1602@gmail.com>
2023-09-27 01:24:15 +0530

    DFS Solution

--- Graphs/Eventual-Safe-states.java
+++ Graphs/Eventual-Safe-states.java
@@ -1,12 +1,10 @@
-import java.awt.List;
-import java.util.ArrayList;
-
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
+    public List<Integer> eventualSafeNodes(int[][] edges) {
 
         List<List<Integer>> adj = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
-        boolean[] visited = new boolean[graph.length];
+        boolean[] visited = new boolean[edges.length];
+        boolean[] pathVisited = new boolean[edges.length];
         // create adjacency list
         for(int i =0;i<edges.length;i++){
             adj.add(new ArrayList<>());
@@ -15,22 +13,30 @@ class Solution {
             }
         }
 
-        for(int i =0;i<n;i++){
+        for(int i =0;i<edges.length;i++){
             if(!visited[i]){
-                checkCycle(i,new boolean[graph.length],visited,adj,result);
+                checkCycle(i,pathVisited,visited,adj,result);
             }
         }
-
+        Collections.sort(result);
         return result;
     }
 
     boolean checkCycle(int node,boolean[] pathVisited,boolean[] visited,List<List<Integer>> adj,List<Integer> result){
-        if(!pathVisited[node]){
-            return true;
-        }
-
         pathVisited[node] = true;
         visited[node] = true;
-        
+        for(int i : adj.get(node)){
+            if(!visited[i]){
+                if(checkCycle(i,pathVisited,visited,adj,result)){
+                    return true;
+                }
+            }
+            else if(pathVisited[i]){
+                return true;
+            }
+        }
+        pathVisited[node] = false;
+        result.add(node);
+        return false;
     }
 }
