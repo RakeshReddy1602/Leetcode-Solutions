ed546d0aea9aa2843210aa56b3c61adca9b09783
RakeshReddy1602 <rakeshb1602@gmail.com>
2023-10-04 00:59:49 +0530

    Recusrive Solution

--- Recursion/Letter-Combinations-Of-a-Phone-Number.java
+++ Recursion/Letter-Combinations-Of-a-Phone-Number.java
@@ -1,18 +1,22 @@
-import java.util.*;
 class Solution {
     public List<String> letterCombinations(String digits) {
+        if(digits.length() == 0){
+            return new ArrayList<>();
+        }
         List<String> result = new ArrayList<>();
         String[] keyboard = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        getCombinations("", digits.length(),keyboard,result);
+        getCombinations(0,"", digits,keyboard,result);
         return result;
     }
 
-    void getCombinations(String currCombo,int target,String[] keyboard,List<String> result){
-        if(currCombo.length() == target){
-            result.add(currCombo);
-            return ;
+    void getCombinations(int index,String path,String digits,String[] keyboard,List<String> result){
+        if(path.length() == digits.length()){
+            result.add(path);
+            return ; 
         }
-
-        
+        String currString = keyboard[digits.charAt(index) -'0'];
+        for(int i =0;i<currString.length();i++){
+            getCombinations(index+1, path + currString.charAt(i), digits, keyboard, result);  
+        }
     }
 }
